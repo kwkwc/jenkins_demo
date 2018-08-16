0d92cb6b0a0bb72b5c368e21489ee9b7369091f8
kwkw <wei497896143@yeah.net>
2018-08-16 10:09:25 +0800

    feat: update deploy script

--- deploy_jenkins.sh
+++ deploy_jenkins.sh
@@ -3,15 +3,20 @@
 # Program:
 #     deploy jenkins
 # Version:
-#     1.0.0
+#     1.0.1
 # History:
 #     Created on 2018/08/15
-#     Last modified on 2018/08/15
+#     Last modified on 2018/08/16
 
 # 容器名称
 CONTAINER="jenkins_demo"
 # 镜像名称（以日期时间为镜像标签，防止重复）
 IMAGE=$CONTAINER":"$(date -d "today" +"%Y%m%d_%H%M%S")
+
+# 删除滚动更新残留的容器
+docker rm `docker ps -a | grep -w $CONTAINER"_"$CONTAINER | awk '{print $1}'`
+# 强制删除滚动更新残留的镜像
+docker rmi --force `docker images | grep -w $CONTAINER | awk '{print $3}'`
 
 # 创建新镜像
 docker build -t $IMAGE . && \
